07fff7162cad60e69513222666edd4babb8a040a
Robert Simpson <ttfnrob@gmail.com>
2010-06-19 22:33:10 +0100

    Updated Home with a template

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :satellites do |satellites|
     satellites.resources :positions
   end
+  map.root :controller => 'home', :action => 'index'
 end
