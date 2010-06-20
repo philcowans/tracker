8f496ae0f76f29d0014f7d8b6ce40095b5a011f1
Ubuntu <ubuntu@ip-10-226-178-48.eu-west-1.compute.internal>
2010-06-20 09:52:54 +0000

    Added index method for positions, and demo jsonp client.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,4 +1,37 @@
 class PositionsController < ApplicationController
+  def index
+    satellite = Satellite.new(params[:satellite_id].to_i)
+
+    base_time = Time.now
+    interval = params[:interval].to_i
+    length = params[:length].to_i
+
+    geo_coordinates = []
+    
+    0.upto length do |i|
+      geo_coordinates[i] = satellite.geo_coordinates(Time.now + i * interval.minutes)
+    end
+
+    respond_to do |format|
+      format.json do
+        json_response = {
+          :positions => geo_coordinates.map do |c| {
+            :satellite => satellite.name,
+            :latitude => c.lat,
+            :longitude => c.lon,
+            :altitude => c.alt
+            }
+          end
+        }.to_json
+        if params[:callback]
+          render :json => "#{params[:callback]}(#{json_response})"
+        else
+          render :json => json_response
+        end
+      end
+    end
+  end
+
   def show
     satellite = Satellite.new(params[:satellite_id].to_i)
     timestamp_ref = TimestampRef.new(params[:id])
